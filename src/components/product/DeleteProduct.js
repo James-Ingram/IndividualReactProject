95bd94aed2954f9dbed703966ed88ee7c34f2819
smurfjames101 <smurfjames@gmail.com>
2019-03-05 23:01:22 +0000

    Added Delete Functionality

--- src/components/product/DeleteProduct.js
+++ src/components/product/DeleteProduct.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import Products from '../Products';
 import '../../App.css';
+import axios from 'axios';
+import MessageDisplay from './MessageDisplay';
 class DeleteProduct extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: "",
+            data: "Status Unkown"
+        }
+    }
+    onChange = (e) => {
+        this.setState({ input: e.target.value });
+    }
+    deleteProduct = (e) => {
+        axios.delete(`http://localhost:8080/JamesIndividualProject/api/product/deleteProduct/${this.state.input}`)
+            .then(r => this.setState({ data: r.data }))
+        .catch(e => console.log(e))
+    }
     render() {
         return (
             <div>
@@ -10,9 +27,12 @@ class DeleteProduct extends Component {
                     What Do You Want To <span style={{ color: "red" }}>Delete</span>?
 		            <div>
                         <span style={{ color: "red" }}>Delete</span> The Product Where The productId Matches <input
-                            id="productDeleteInput" type="text" maxLength="20" />
-                        <button id="button" type="button">Submit</button>
+                            id="productDeleteInput" type="text" maxLength="20" onChange={this.onChange} />
+                        <button id="button" type="button" onClick={this.deleteProduct}>Submit</button>
                     </div>
+                    <MessageDisplay
+                        message={this.state.data}
+                    />
                 </div>
             </div>
         );
